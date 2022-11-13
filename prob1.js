9ac2d778e3fb82a1270ec50bfcbb7bf96613ab10
Ben Luerding <bluerding@gmail.com>
2022-11-13 11:04:31 -0600

    Add 2 challenges

---
+++ prob1.js
@@ -0,0 +1,6 @@
+var names = ["Ben", "Maya", "Whitney", "Bo"]
+for (i = 0; i < names.length; i++){
+    if(names[i].length === 4){
+        console.log(names[i])
+    }
+}
---
+++ prob2.js
@@ -0,0 +1,4 @@
+var places = ["StL", "WebSTeR", "KIRkwooD"]
+for (i = 0; i < places.length; i++){
+    console.log(places[i].toLowerCase())
+} 
