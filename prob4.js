54619d2689f21bab57d128fc572230b14ca76de5
Ben Luerding <bluerding@gmail.com>
2022-11-13 11:41:18 -0600

    Add prob4

---
+++ prob4.js
@@ -0,0 +1,14 @@
+var words = ["look", "looking", "swimming", "swim"]
+for (i = 0; i < words.includes; i++){
+    if (words[i].includes === "ing") {
+        console.log(words[i])
+    }
+}
+
+// I can not get this one to work. I am not sure if I am using the wrong methods.
+// I want to print every word that contains the letters "ing", and thought this
+// was the method that fit best to do that, but when I run it, I get nothing.
+// I have also tried using .length and nothing returns still.
+
+// I wanted to leave this in for potential feedback on what I could do to have
+// this work.
